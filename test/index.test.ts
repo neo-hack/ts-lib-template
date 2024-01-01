b54c05cc89a609bd4e79fa15f0533f5135152397
JW <jiangweixian1994@gmail.com>
2024-01-01 14:02:53 +0800

    :white_check_mark:  vitest

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,9 @@
 import welcome from '../src'
+import { describe, test, expect, vi } from 'vitest'
 
 describe('index', () => {
   test('hello world should console', () => {
-    console.log = jest.fn()
+    console.log = vi.fn()
     welcome()
     expect(console.log).toHaveBeenCalledWith('hello world')
   })
